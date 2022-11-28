57138f0d5f2ad2e42cd0058f74c1967240118a9b
Angel Grablev <agrublev@gmail.com>
2022-11-29 01:29:07 +0200

    upgraded to use 0'ss

--- src/Reader.js
+++ src/Reader.js
@@ -1,13 +1,20 @@
 import NPS from "net-promoter-score";
 import React, { Component } from "react";
-import { observable } from "mobx";
+import { observable, toJS } from "mobx";
 import { observer } from "mobx-react";
 import moment from "moment-mini";
 
+// let mMonth = localStorage.getItem("monthResults");
+// let mYear = localStorage.getItem("yearResults");
+const median = arr => {
+    const mid = Math.floor(arr.length / 2),
+        nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
 class Reader extends Component {
     @observable value = "file name";
-    @observable yearResults = null;
-    @observable monthResults = null;
+    @observable yearResults = null; //mMonth !== undefined ? JSON.parse(mMonth) : null;
+    @observable monthResults = null; //mYear !== undefined ? JSON.parse(mYear) : null;
 
     render() {
         return (
@@ -39,7 +46,7 @@ class Reader extends Component {
                     <div className={"results"}>
                         <h2>YEARS</h2>
                         <div className={"scores"}>
-                            {Object.keys(this.yearResults).map((year) => {
+                            {Object.keys(this.yearResults).map(year => {
                                 return (
                                     <div className={"scoreWrap"}>
                                         <div className={"year"}>{year}</div>
@@ -57,28 +64,46 @@ class Reader extends Component {
                         <h2>MONTHS</h2>
                         {Object.keys(this.monthResults)
                             .sort((a, b) => parseInt(b.split("-")[0]) - parseInt(a.split("-")[0]))
-                            .map((year) => {
+                            .map(year => {
                                 return (
                                     <div>
                                         <h2>YEAR {year}</h2>
                                         <div className={"scores"}>
                                             {Object.keys(this.monthResults[year])
                                                 .sort((a, b) => parseInt(a) - parseInt(b))
-                                                .map((month) => {
+                                                .map(month => {
                                                     return (
-                                                        <div className={"scoreWrap"}>
+                                                        <div
+                                                            className={"scoreWrap"}
+                                                            title={`Median: ${this.monthResults[year][month].median}`}
+                                                        >
                                                             <div className={"year"}>{month}</div>
                                                             <div
                                                                 className={"score"}
                                                                 style={{
-                                                                    height: `${this.monthResults[year][month].score}%`,
+                                                                    height: `${
+                                                                        this.monthResults[year][
+                                                                            month
+                                                                        ].nps < 20
+                                                                            ? 10
+                                                                            : this.monthResults[
+                                                                                  year
+                                                                              ][month].nps
+                                                                    }%`
                                                                 }}
                                                             >
+                                                                {this.monthResults[year][month].nps}
+                                                            </div>
+                                                            <div className={"score average"}>
                                                                 {
                                                                     this.monthResults[year][month]
-                                                                        .score
+                                                                        .average
                                                                 }
+                                                                /10
                                                             </div>
+                                                            {/*<div className={"score median"}>*/}
+                                                            {/*    {this.monthResults[year][month].median}*/}
+                                                            {/*</div>*/}
                                                         </div>
                                                     );
                                                 })}
@@ -92,14 +117,14 @@ class Reader extends Component {
         );
     }
 
-    updateFile = (evt) => {
+    updateFile = evt => {
         const updatedFileName = evt.target.value.split("\\").pop();
         // setFileName(updatedFileName);
         this.value = updatedFileName;
         this.readFile(evt.target);
     };
 
-    readFile = (fileInput) => {
+    readFile = fileInput => {
         const reader = new FileReader();
         const file = fileInput.files[0];
         console.clear();
@@ -110,12 +135,12 @@ class Reader extends Component {
                 this.showNPS(reader.result);
             };
 
-            reader.onerror = function () {
+            reader.onerror = function() {
                 console.log(reader.error);
             };
         }
     };
-    showNPS = (file) => {
+    showNPS = file => {
         let years = {};
         let months = {};
         let mat = file.match(/<tr(.[\s\S]*?)>(.[\s\S]*?)<\/tr>/g);
@@ -124,7 +149,7 @@ class Reader extends Component {
             let cols = a.match(/<td(.[\s\S]*?)<\/td>/g);
 
             if (cols) {
-                cols = cols.map((e) =>
+                cols = cols.map(e =>
                     e
                         .replace(/<td(.*?)>/g, "")
                         .replace(/<\/td(.*?)>/g, "")
@@ -144,32 +169,30 @@ class Reader extends Component {
                 months[month].push({ score: cols[1], date: cols[2] });
             }
         });
+        console.log("RES", years, months);
+
         // console.log("--- INFO ", scores);
         this.monthResults = this.calcMonths(months);
         this.yearResults = this.calcYear(years);
+        localStorage.setItem("monthResults", JSON.stringify(this.monthResults));
+        localStorage.setItem("yearResults", JSON.stringify(this.yearResults));
     };
-    calcYear = (years) => {
+    calcYear = years => {
         let yearsScores = {};
 
-        Object.keys(years).forEach((year) => {
+        Object.keys(years).forEach(year => {
             let yyy = years[year];
-            let firstScores = new NPS(
-                yyy
-                    .map((e) => parseInt(e.score))
-                    .filter((e) => {
-                        // console.log(e);
+            let scoreList = yyy.map(e => parseInt(e.score)).filter(e => e > 0 && e < 11);
 
-                        return e > 0 && e < 11;
-                    })
-            );
+            let firstScores = new NPS(scoreList);
             yearsScores[year] = firstScores;
         });
         return yearsScores;
     };
-    calcMonths = (months) => {
+    calcMonths = months => {
         let years = {};
 
-        Object.keys(months).forEach((name) => {
+        Object.keys(months).forEach(name => {
             let year = name.split("-")[0];
             let month = name.split("-")[1];
             if (years[year] === undefined) {
@@ -178,21 +201,24 @@ class Reader extends Component {
             if (years[year][month] === undefined) {
                 years[year][month] = {};
             }
-            let firstScores = new NPS(
-                months[name]
-                    .map((e) => parseInt(e.score))
-                    .filter((e) => {
-                        // console.log(e);
-
-                        return e > 0 && e < 11;
-                    })
-            );
-            years[year][month] = firstScores;
+            let scoreList = months[name]
+                .map(e => parseInt(e.score))
+                .filter(e => {
+                    if (e > -1 && e < 11) {
+                        return true;
+                    }
+                    console.log("DIDNT", e);
+                    return false;
+                });
+            let { score: nps } = new NPS(scoreList);
+            years[year][month].nps = nps;
+            years[year][month].average = average(...scoreList);
+            years[year][month].median = median(scoreList);
         });
-        console.info("Console --- ", years);
 
         return years;
     };
 }
+const average = (...nums) => Math.round(nums.reduce((acc, val) => acc + val, 0) / nums.length);
 
 export default observer(Reader);
